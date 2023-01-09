2ce2b645c65d274f639c0d44eb701cfeebc6776b
OG-Gopher <57696915+OG-Gopher@users.noreply.github.com>
2023-01-09 13:34:33 -0700

    main
    main

---
+++ DX12.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace SpeedGameLauncherWPF
+{
+    /// <summary>
+    /// Interaction logic for DX12.xaml
+    /// </summary>
+    public partial class DX12 : Window
+    {
+        public DX12()
+        {
+            InitializeComponent();
+        }
+
+        private async void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Close all windows
+            Application.Current.Windows.OfType<Window>().ToList().ForEach(x => x.Close());
+
+            // Open the executable file "SpeedGame" with the launch argument "-dx12"
+            System.Diagnostics.Process.Start("SpeedGame", "-dx12");
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpeedGameLauncherWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseAllWindows()
+        {
+            for (int intCounter = App.Current.Windows.Count - 1; intCounter > 0; intCounter--)
+                App.Current.Windows[intCounter].Close();
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private async void PlayGameDX12_Click(object sender, RoutedEventArgs e)
+        {
+
+            // MessageBox.Show("Project Speed 2 is not performance friendly when using DirectX12! It is recommended to play the game using the default DirectX11 renderer!");
+            
+            DX12 SGLWin = new DX12();
+            SGLWin.Show();
+            /* await Task.Run(() =>
+            Close()); */
+        }
+
+        private async void PlayGameDefault_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"SpeedGame.exe");
+            await Task.Run(() =>
+                Close());
+        }
+    }
+}
